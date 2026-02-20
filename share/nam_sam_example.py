ba151f9ade6710195175dfef2b7b05ba8008072e
shaanjanardhan <shaanjanardhan@gmail.com>
2026-02-19 16:29:07 -0800

    Update example config

--- share/nam_sam_example.py
+++ share/nam_sam_example.py
@@ -23,7 +23,7 @@ params_sam_24hr = dict(
 
 params_nam_12hr = dict(
         # directory of input forecast
-        input_file = '/home/disk/rhodium/nacc/forecasts/hpx64_coupled-dlwp-olr_1-to-1_seed0_12h-AR+hpx64_coupled-dlom-olr_unet_dil-112_double_restart/atmos_100-year_JanInit_z1000_ll.nc ',
+        input_file = '/home/disk/rhodium/nacc/forecasts/hpx64_coupled-dlwp-olr_1-to-1_seed0_12h-AR+hpx64_coupled-dlom-olr_unet_dil-112_double_restart/atmos_100-year_JanInit_z1000_ll.nc',
         # Northern or Southern Annular mode
         mode = 'NAM',
         # We save the computed nam or sam pattern to an itermediate cache before plotting.
@@ -33,7 +33,7 @@ params_nam_12hr = dict(
 
     )
 params_sam_12hr = dict(
-        input_file = '/home/disk/rhodium/nacc/forecasts/hpx64_coupled-dlwp-olr_1-to-1_seed0_12h-AR+hpx64_coupled-dlom-olr_unet_dil-112_double_restart/atmos_100-year_JanInit_z500_ll.nc ',
+        input_file = '/home/disk/rhodium/nacc/forecasts/hpx64_coupled-dlwp-olr_1-to-1_seed0_12h-AR+hpx64_coupled-dlom-olr_unet_dil-112_double_restart/atmos_100-year_JanInit_z500_ll.nc',
         mode = 'SAM',
         output_cache = '/home/disk/brume/shaanj/gradient_dissent/DLESyM/scratch/nam_sam/1in1out12hr_cache_sam.nc',
         output_plot_file = '/home/disk/brume/shaanj/gradient_dissent/DLESyM/scratch/nam_sam/1in1out12hr_map_sam.png'
@@ -43,9 +43,9 @@ params_sam_12hr = dict(
 
 
 if __name__ == "__main__":
-    nam_sam_main(**params_nam_24hr)
-    nam_sam_main(**params_sam_24hr)
     nam_sam_main(**params_nam_12hr)
     nam_sam_main(**params_sam_12hr)
+    nam_sam_main(**params_nam_24hr)
+    nam_sam_main(**params_sam_24hr)
 
 
